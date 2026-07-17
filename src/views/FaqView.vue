34b168841ea466b7a7ca9733d7beeb4fa22afaa9
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-07-17 10:23:05 +0800

    fix format

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="mt-[-86px] bg-vt-gray-800 pt-20 text-white sm:mt-[-90px]">
     <div class="mx-auto max-w-3xl py-12">
-      <h1 class="p-2 mb-8 text-4xl font-bold sticky top-[80px] sm:top-[88px] bg-vt-gray-800 z-10">{{ t('faq.title') }}</h1>
+      <h1 class="sticky top-[80px] z-10 mb-8 bg-vt-gray-800 p-2 text-4xl font-bold sm:top-[88px]">{{ t('faq.title') }}</h1>
 
       <section class="mb-12">
         <p class="mb-6 leading-relaxed">{{ t('faq.description') }}</p>
